ed52b1dc7185b364ef378084d02140699bc0c151
Sergey Galushka <fotoword.ua@gmail.com>
2025-12-07 18:32:12 +0100

    HW18-pagepagination

--- core/settings.py
+++ core/settings.py
@@ -172,8 +172,10 @@ REST_FRAMEWORK = {
         "rest_framework.filters.OrderingFilter",
     ],
 
-    # Выбираю глобальную безопасную CursorPagination
-    "DEFAULT_PAGINATION_CLASS": "manager.pagination.DefaultCursorPagination",
+    # # Выбираю глобальную безопасную CursorPagination
+    # "DEFAULT_PAGINATION_CLASS": "manager.pagination.DefaultCursorPagination",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
 }
 
 SIMPLE_JWT = {
